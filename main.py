f2c3895d4614629a6de632155de2563996929775
t1edtke <136181851+t1edtke@users.noreply.github.com>
2025-06-27 15:06:53 +0200

    wip

---
+++ main.py
@@ -0,0 +1,92 @@
+import asyncio
+
+import inquirer
+import skinport
+
+
+async def main():
+    while True:
+        endpoint = inquirer.list_input(
+            'What do you want to do?',
+            choices=[
+                ('Get items', 0),
+                ('Get sales history', 1),
+                ('Get item out of stock', 2),
+                ('Get account transactions', 3),
+                ('Subscribe to sale feed', 4),
+                ('Exit', 5)
+            ])
+
+        if endpoint == 0:
+            items = await skinport.Client().get_items()
+
+            text = inquirer.text(
+                'Enter a search term (leave empty for all items)',
+                default='',
+            )
+
+            market_hash_names = [(item.market_hash_name, item) for item in items if text.lower() in item.market_hash_name.lower()]
+
+            if not market_hash_names:
+                print('No items found.')
+                continue
+
+            item = inquirer.list_input(
+                'Select an item to get details',
+                choices=market_hash_names,
+            )
+            print(repr(item))
+
+        elif endpoint == 1:
+            items = await skinport.Client().get_sales_history()
+
+            text = inquirer.text(
+                'Enter a search term (leave empty for all items)',
+                default='',
+            )
+
+            market_hash_names = [(item.market_hash_name, item) for item in items if text.lower() in item.market_hash_name.lower()]
+
+            if not market_hash_names:
+                print('No items found.')
+                continue
+
+            item = inquirer.list_input(
+                'Select an item to get sales history',
+                choices=market_hash_names,
+            )
+            print(repr(item))
+
+        elif endpoint == 2:
+            items = await skinport.Client().get_sales_out_of_stock()
+
+            text = inquirer.text(
+                'Enter a search term (leave empty for all items)',
+                default='',
+            )
+
+            market_hash_names = [(item.market_hash_name, item) for item in items if text.lower() in item.market_hash_name.lower()]
+
+            if not market_hash_names:
+                print('No items found.')
+                continue
+
+            item = inquirer.list_input(
+                'Select an item to get details',
+                choices=market_hash_names,
+            )
+            print(repr(item))
+
+        elif endpoint == 3:
+            print('Not implemented')
+
+        elif endpoint == 4:
+            print('Not implemented')
+
+        elif endpoint == 5:
+            print('Exiting...')
+            break
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
